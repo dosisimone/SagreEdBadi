4a77269f5a8073d64cfae2084c0719d0fee6fb66
Simone Dosi <dousi96@hotmail.it>
2020-03-29 20:51:07 +0200

    Fixati i file di progetto non necessari

--- src/sagreEdBadi/servlet/BadiServlet.java
+++ src/sagreEdBadi/servlet/BadiServlet.java
@@ -9,17 +9,12 @@ import javax.servlet.http.HttpServletResponse;
 
 public class BadiServlet extends HttpServlet {
 
-	 /**
-     * this life-cycle method is invoked when this servlet is first accessed
-     * by the client
-     */
-    public void init(ServletConfig config) {
+	private static final long serialVersionUID = 1L;
+
+	public void init(ServletConfig config) {
         System.out.println("Servlet is being initialized");
     }
  
-    /**
-     * handles HTTP GET request
-     */
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws IOException {
  
@@ -28,29 +23,6 @@ public class BadiServlet extends HttpServlet {
         writer.flush();
     }
  
-    /**
-     * handles HTTP POST request
-     */
-    public void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws IOException {
-        String paramWidth = request.getParameter("width");
-        int width = Integer.parseInt(paramWidth);
- 
-        String paramHeight = request.getParameter("height");
-        int height = Integer.parseInt(paramHeight);
- 
-        long area = width * height;
- 
-        PrintWriter writer = response.getWriter();
-        writer.println("<html>Area of the rectangle is: " + area + "</html>");
-        writer.flush();
- 
-    }
- 
-    /**
-     * this life-cycle method is invoked when the application or the server
-     * is shutting down
-     */
     public void destroy() {
         System.out.println("Servlet is being destroyed");
     }
